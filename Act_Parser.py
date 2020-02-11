e8fceccbc2f76d1b61c2885a1df66064c44bd60b
manoraaju <44794485+manoraaju@users.noreply.github.com>
2020-02-12 00:21:08 +0800

    #New XML # Completed injection and flooding

--- Act_Parser.py
+++ Act_Parser.py
@@ -113,10 +113,11 @@ class process_conditions():
         self.name = name
         self.xml_dict = matrix
         self.attack_params_dict = {}
+        self.function_init = {}
 
     def flatten(self, l):
         for el in l:
-            if isinstance(el, collections.Iterable) and not isinstance(el, (str, bytes)):
+            if isinstance(el, collections.abc.Iterable) and not isinstance(el, (str, bytes)):
                 yield from self.flatten(el)
             else:
                 yield el
@@ -129,7 +130,7 @@ class process_conditions():
         raw_global_vars = root_xml.xpath("/nta/declaration/text()")[0]
         raw_global_vars = raw_global_vars.replace("\n", "")
         raw_global_vars = raw_global_vars.split(";")
-        variable_types = ["int", "bool"]
+        variable_types = ["int", "bool", "clock"]
         raw_global_vars = [var for var in raw_global_vars for type in variable_types if type in var]
         self.global_vars_dict = {}
         for type in variable_types:
@@ -138,12 +139,13 @@ class process_conditions():
             temp_dict = {}
             for temp in temp_list:
                 temp = temp.split("=")
-                temp_dict["glo_"+temp[0]] = temp[1]
+                if len(temp)==2:
+                    temp_dict["glo_"+temp[0]] = temp[1]
+                if len(temp)==1:
+                    temp_dict["glo_" + temp[0]] = ""
             self.global_vars_dict[type] = temp_dict
         print("end")
 
-    def to_global_vars(self):
-        print(1)
 
     def get_variables_from_operator(self, string_list):
         return_list = []
@@ -237,6 +239,7 @@ class process_conditions():
             for c2 in range(len(assignment_list[c1])):
                 operator = assignment_list[c1][c2]
                 var_temp = re.sub(" ","", operator)
+                var_temp_1 = var_temp[:]
                 var_temp = re.split(operator_split, var_temp)
                 flag_1 = False
                 if len(var_temp) == 2:
@@ -245,187 +248,140 @@ class process_conditions():
                     if self.is_numeric(var_temp[1]):
                         assignment_list[c1][c2] = var_temp[0] + " == " + var_temp[1]
                         flag_1 = True
-                    #Condition II: if expression is an arithmetic operation, verify the condition
-                    # is correct by equating the local variable to the global variable
-                    if self.is_arithmetic(var_temp[0], var_temp[1]) and flag_1 != True:
-                        assignment_list[c1][c2] = var_temp[0] + " == glo_" + var_temp[0] + "+1"
-                        flag_1 = True
-                    #Condition III: if expression is an assignment operation. use local variable
+                    # Condition III: if expression is an assignment operation. use local variable
                     # declaration and perform equality check
-                    if self.is_bool(var_temp[1]) and flag_1 != True:
+                    elif self.is_bool(var_temp[1]) and flag_1 != True:
                         assignment_list[c1][c2] = var_temp[0] + " == " + var_temp[1]
                         flag_1 = True
+                    #Condition II: if expression is an arithmetic operation, verify the condition
+                    # is correct by equating the local variable to the global variable
+                    elif self.is_counter(var_temp_1) and flag_1 != True:
+                        assignment_list[c1][c2] = var_temp[0] + " == glo_" + var_temp[0] + "+1"
+                        flag_1 = True
+                    #else:
+                    #    assignment_list[c1][c2] = assignment_list[c1][c2].replace(var_temp[0], "glo_"+var_temp[0])
+                    #    assignment_list[c1][c2] = assignment_list[c1][c2].replace(var_temp[1], "glo_" + var_temp[1])
                 if len(var_temp) == 1:
                     if self.is_counter(var_temp[0]):
                         var_temp = re.sub('[++,+1,+ 1]', '', var_temp[0])
                         assignment_list[c1][c2] = var_temp + " == glo_" + var_temp + " +1"
         return assignment_list
 
+    def get_update_variables(self, attack_type):
+        for cond in range(len(self.attack_params_dict[attack_type])):
+            temp_list = []
+            for key in self.attack_params_dict[attack_type][cond]:
+                if key == "assignment":
+                    for v in self.attack_params_dict[attack_type][cond][key]:
+                        var_temp = v.replace(" ", "")
+                        var_temp = re.split('==', var_temp)
+                        # Rule #1
+                        if self.is_bool(var_temp[1]):
+                            if var_temp[1] in "True true":
+                                temp_list.append("glo_"+var_temp[0]+" = False;")
+                            else:
+                                temp_list.append("glo_" + var_temp[0] + " = True;")
+                        # Rule #2
+                        if self.is_numeric(var_temp[1]):
+                            temp_list.append("glo_"+var_temp[0] + " = "+var_temp[0]+";")
+                        # Rule #3
+                        if self.is_counter(v):
+                            temp_list.append("glo_"+var_temp[0] + " = "+var_temp[0]+";")
+            self.attack_params_dict[attack_type][cond]["update"] = temp_list
+
+
     def get_parameters(self, attack_type):
         #Making a guard dictionary and adding to main attack_params_dict
         guard_list = self.get_guard()
         guard_list = self.make_global_guard(guard_list)
-        cond_dict = {}
+        cond_list = []
         for c in range(len(guard_list)):
             guard_dict = {}
-            cond_dict["cond_" + str(c)] = guard_dict
-            cond_dict["cond_"+str(c)]["guard"] = guard_list[c]
-            self.attack_params_dict[attack_type] = cond_dict
+            cond_list.append(guard_dict)
+            cond_list[(c)]["guard"] = guard_list[c]
+        self.attack_params_dict[attack_type] = cond_list
         assignment_list = self.get_assignment()
         assignment_list = self.make_assignment_rules(assignment_list)
         #Sub-conditions
         for c in range(len(assignment_list)):
             assignment_dict = {}
-            cond_dict["cond_" + str(c)]["assignment"] = assignment_list[c]
-            self.attack_params_dict[attack_type] = cond_dict
-        print(1)
+            cond_list[(c)]["assignment"] = assignment_list[c]
+        self.attack_params_dict[attack_type] = cond_list
+        update_list = self.get_update_variables(attack_type)
 
-
-# Only for busbar_IED1
-processed_variables_dict = {}
-xml_dict_list = xml_dict["busbar_IED1"].xml_values
-processed_variables_dict["busbar_IED1"] = process_conditions("busbar_IED1", xml_dict_list)
-guard_list = processed_variables_dict["busbar_IED1"].get_parameters(attack_type="Injection")
-processed_variables_dict["busbar_IED1"].get_global_variables()
-processed_variables_dict["busbar_IED1"].make_negations(guard_list)
-
-
-class create_variables():
-    def __init__(self, name, matrix_list, no_of_cond, negated_guard_dict):
-        self.name = name
-        self.dict_matrix = matrix_list
-        self.no_of_cond = no_of_cond
-        self.negated_guard_dict = negated_guard_dict
-
-    def flatten(self, l):
-        for el in l:
-            if isinstance(el, collections.abc.Iterable) and not isinstance(el, (str, bytes)):
-                yield from self.flatten(el)
-            else:
-                yield el
-
-    def get_raw_list(self):
-        all_values = list(self.dict_matrix.values())
-        merged_list = list(self.flatten(all_values))
-        # drop duplicates
-        final_list = list(dict.fromkeys(merged_list))
-        return final_list
-
-    def is_bool(self, b):
-        b_list = ["True", "TRUE", "true", "False", "FALSE", "false"]
-        try:
-            if b in b_list:
-                return True
-        except:
-            return False
-
-    def is_numeric(self, b):
-        if b.isnumeric():
-            return True
-        else:
-            return False
-
-    def is_counter(self, string):
-        container = ["++", "+1", "+ 1"]
-        try:
-            for c in container:
-                if c in string:
-                    return True
-        except:
-            return False
-
-    def get_global_variables(self):
-        temp_var_list = []
-        for var in self.final_local_variables:
-            temp_var_list.append("glo_"+var)
-        self.final_global_variables = temp_var_list.copy()
-
-    def get_local_variables(self):
+    def get_function_init(self, attack_type):
         final_var_list = []
         function_init = []
-        final_list = self.get_raw_list()
-        #split_final_list = self.string_split(final_list)
+        final_list = []
+        for cond in self.attack_params_dict[attack_type]:
+            for key in cond:
+                if key == "assignment":
+                    final_list.extend(cond[key])
+        final_list = list(dict.fromkeys(final_list))
+        # split_final_list = self.string_split(final_list)
         for st in final_list:
             var_temp = st.strip()
             var_temp = var_temp.replace(" ", "")
             var_temp = re.split('<(?!=)|<=|==|=(?!=)|>(?!=)|>=', var_temp)
             if len(var_temp) == 2:
-                #Check if the string after operator symbol is numeric or boolean
+                # Check if the string after operator symbol is numeric or boolean
                 if self.is_bool(var_temp[1]):
                     final_var_list.append(var_temp[0])
-                    function_init.append(var_temp[0]+": bool")
+                    function_init.append(var_temp[0] + ": bool")
                 elif self.is_numeric(var_temp[1]):
                     final_var_list.append(var_temp[0])
-                    function_init.append(var_temp[0]+": double")
+                    function_init.append(var_temp[0] + ": double")
                 elif self.is_counter(var_temp[1]):
                     final_var_list.append(var_temp[0])
-                    function_init.append(var_temp[0]+": count")
+                    function_init.append(var_temp[0] + ": count")
                 else:
                     final_var_list.append(var_temp[0])
-                    function_init.append(var_temp[0]+": double")
+                    function_init.append(var_temp[0] + ": double")
                     final_var_list.append(var_temp[1])
-                    function_init.append(var_temp[1]+": double")
+                    function_init.append(var_temp[1] + ": double")
             if len(var_temp) == 1:
                 if self.is_counter(var_temp[0]):
                     final_var_list.append(var_temp[0].strip("++"))
-                    function_init.append((var_temp[0].strip("++"))+": count")
+                    function_init.append((var_temp[0].strip("++")) + ": count")
 
-        self.final_local_variables = final_var_list
-        self.function_init = ", ".join(function_init)
+        #self.final_local_variables = final_var_list
+        self.function_init[attack_type] = ", ".join(function_init)
 
-## The for loop in dictionary is to be created. For time being only one variable is being used.
-global_var_dict = {}
-v1 = "busbar_IED"
-v2 = negation_dict[v1].dict_matrix
-v3 = negation_dict[v1].no_of_cond
-v4 = negation_dict[v1].negated_guard_dict
-global_var_dict[v1] = create_variables(v1, v2, v3, v4)
-global_var_dict[v1].get_local_variables()
-global_var_dict[v1].get_global_variables()
+# Only for busbar_IED1
+processed_variables_dict = {}
+xml_dict_list = xml_dict["busbar_IED1"].xml_values
+processed_variables_dict["busbar_IED1"] = process_conditions("busbar_IED1", xml_dict_list)
+processed_variables_dict["busbar_IED1"].get_global_variables()
+processed_variables_dict["busbar_IED1"].get_parameters(attack_type="Inject & Flood")
+processed_variables_dict["busbar_IED1"].get_function_init(attack_type = "Inject & Flood")
 
-print("end")
 
-class injection_attack_variables():
-    def __init__(self, name, matrix_list, no_of_cond, negated_guard_dict, final_global_variables,func_init):
+class create_function_bro():
+    def __init__(self, name, params_list, function_init, global_vars_dict):
         self.name = name
-        self.dict_matrix = matrix_list
-        self.no_of_cond = no_of_cond
-        self.negated_guard_dict = negated_guard_dict
-        self.final_global_variables = final_global_variables
-        self.function_init = func_init
+        self.params_list = params_list
+        self.function_init = function_init
+        self.global_vars_dict= global_vars_dict
+        #IF - Inject & Flood
+        self.mapping_IF = {"pre_cond_1":"guard",
+                           "pre_cond_2":"assignment",
+                           "pre_cond_3": "update"}
+        #Test
+        self.create_inject_flood_function((2,17))
+
+    def insert_precondition_IF(self, templater, parameters):
+        template = templater[:]
+        for n1 in range(len(template)):
+            for key in template[n1]:
+                try:
+                    print(template[n1][key])
+                    print(parameters[n1][self.mapping_IF[key]])
+                    template[n1][key] =  1#" && ".join(parameters[c1][self.mapping_IF[key]])
+                except:
+                    continue
 
 
-class create_functions_bro():
-    def __init__(self, name, matrix_list, no_of_cond, negated_guard_dict, final_global_variables, func_init):
-        self.name = name
-        self.dict_matrix = matrix_list
-        self.no_of_cond = no_of_cond
-        self.negated_guard_dict = negated_guard_dict
-        self.final_global_variables = final_global_variables
-        self.function_init = func_init
-        self.create_injection_function((2,17))
-
-    def injection_preconditions(self, injection_dict, cond_template_dict):
-        # Pre_cond_1
-        for c in range(self.no_of_cond):
-            cond_template_dict[c]["pre_cond_1"] = "("+self.negated_guard_dict["Injection"][c]+")"
-        # Pre_cond_2; check if left is in right
-        for c in range(self.no_of_cond):
-            dict_assignment = self.dict_matrix["assignment"][c]
-            for d in dict_assignment:
-                var_temp = d.strip()
-                var_temp = var_temp.replace(" ", "")
-                var_temp = re.split('<(?!=)|<=|==|=(?!=)|>(?!=)|>=', var_temp)
-                if len(var_temp) > 1:
-                    if var_temp[0] in var_temp[1]:
-                        cond_template_dict[c]["pre_cond_2"] = "({} == glo_{}+1)".format(var_temp[0], var_temp[0])
-                        cond_template_dict[c]["pre_cond_3"] = "glo_{} = {};".format(var_temp[0], var_temp[0])
-
-        return cond_template_dict
-        print("end")
-
-
-    def create_injection_function(self, line):
+    def create_inject_flood_function(self, line, attack_type="Inject & Flood"):
         import_template = open("Injection_template.txt", "r")
         inj_template = import_template.readlines()
         cond_template = []
@@ -434,19 +390,33 @@ class create_functions_bro():
             cond_template.append(inj_template[num])
         cond_template = "".join(cond_template)
         injection_dict = {
-            "func_init": self.function_init,
+            "func_init": self.function_init[attack_type],
             "pre_cond_1":"",
             "pre_cond_2": "",
             "pre_cond_3": "",
-            "next_pre_cond":""
         }
-        cond_template_dict = [injection_dict]*self.no_of_cond
-        con_dict = self.injection_preconditions(injection_dict, cond_template_dict)
+        no_of_cond = len(self.params_list[attack_type])
+        parameters = self.params_list[attack_type]
+        con_dict = []
+        for c in range(no_of_cond):
+            cond_template_dict = injection_dict.copy()
+            for key in cond_template_dict:
+                try:
+                    if key == "pre_cond_3":
+                        cond_template_dict[key] = " ".join(parameters[c][self.mapping_IF[key]])
+                    else:
+                        cond_template_dict[key] =  " && ".join(parameters[c][self.mapping_IF[key]])
+
+                except:
+                    continue
+            con_dict.append(cond_template_dict)
+
+
         make_function = []
         # Function title
         for n1 in range(0, line[0]):
             make_function.append(inj_template[n1]%con_dict[0])
-        for n2 in range(self.no_of_cond):
+        for n2 in range(no_of_cond):
             print(cond_template%con_dict[n2])
             make_function.append(cond_template%con_dict[n2])
         for n3 in range(line[1], len(inj_template)):
@@ -455,14 +425,12 @@ class create_functions_bro():
         print("end")
 
 functions_bro_dict = {}
-v1 = "busbar_IED"
-v2 = global_var_dict[v1].dict_matrix
-v3 = global_var_dict[v1].no_of_cond
-v4 = global_var_dict[v1].negated_guard_dict
-v5 = global_var_dict[v1].final_global_variables
-v6 = global_var_dict[v1].function_init
-functions_bro_dict[v1] = create_functions_bro(v1, v2, v3, v4, v5, v6)
-print("end")
+v1 = "busbar_IED1"
+v2 = processed_variables_dict[v1].attack_params_dict
+v3 = processed_variables_dict[v1].function_init
+v4 = processed_variables_dict[v1].global_vars_dict
+functions_bro_dict[v1] = create_function_bro(v1, v2, v3, v4)
+
 
 
 '''
